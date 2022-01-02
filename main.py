0e43f434e3f528ba7dd772b94a8e244b53bae5bd
AminurAlam <oscorp2004@gmail.com>
2022-01-02 19:23:14 +0530

    some improvements

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-
-
-
-
-
-
-
-
-
-
 import json
 import requests
 import xmltodict
@@ -18,8 +8,8 @@ def request(searchUrl):
 	return metadata
 
 
-def search_artist(base,entity_type,query,limit=5,offset=0):
-	base2 = base + entity_type
+def search_artist(query,limit=5,offset=0):
+	base2 = "https://musicbrainz.org/ws/2/artist"
 	searchUrl = base2 + f"?query={query}&limit={limit}"
 	metadata = request(searchUrl)
 	list = metadata["metadata"]["artist-list"]["artist"]
@@ -28,20 +18,38 @@ def search_artist(base,entity_type,query,limit=5,offset=0):
 		print(type(i),i)
 
 
-def search_rg(base,entity_type,query,limit=5,offset=0):
-	base2 = base + entity_type
+def lookup_rg(mbid,inc="releases"):
+        base2 = "https://musicbrainz.org/ws/2/release-group/"
+        searchUrl = base2 + f"{mbid}?inc={inc}"
+        metadata = request(searchUrl)
+        list = metadata["metadata"]["release-group"]["release-list"]
+        for dic in list:
+                print("""
+id      : {dic["@id"]}
+title   : {dic["title"]}
+date    : {dic["date"]}
+country : {dic["country"]}
+barcode : {dic["barcode"]}""")
+
+
+def search_rg(query,limit=5,offset=0):
+	base2 = "https://musicbrainz.org/ws/2/release-group"
 	searchUrl = base2 + f"?query={query}&limit={limit}"
 	metadata = request(searchUrl)
 	list = metadata["metadata"]["release-group-list"]["release-group"]
 
+	n = 0
 	for dic in list:
-		print(f"""
-	artist : {dic["artist-credit"]["name-credit"]["name"]}
-	title : {dic["title"]}
-	id : {dic["@id"]}
-	type : {dic["@type"]}
-	type-id : {dic["@type-id"]}""")
+		n += 1
+		print(f"""{n}
+artist : {dic["artist-credit"]["name-credit"]["name"]}
+title  : {dic["title"]}
+id     : {dic["@id"]}
+type   : {dic["@type"]}""")
 
+	num = int(input(">choose release-group: "))
+	mbid = list[num-1]["@id"]
+	lookup_rg(mbid)
 
 
 #The API root URL
@@ -57,11 +65,14 @@ query = input(">search query: ")
 #recording, release, release-group, series, work, url
 print("""
 1 : artist
-2 : release group
-3 : lookup
-""")
+2 : release-group
+3 : lookup release-group
+4 :""")
+
+
 entity_type = int(input("search for: "))
 
-if entity_type == 1: print("starting search for artist")
-elif entity_type == 2: search_rg(base,"release-group",query,limit)
+if entity_type == 1: search_artist(query,limit)
+elif entity_type == 2: search_rg(query,limit)
+elif entity_type == 3: lookup_rg(query)
 else: print("the given value doesnt match")
