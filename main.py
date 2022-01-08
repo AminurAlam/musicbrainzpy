7c15005c9f542028cd7f207bb13891ad772fa21d
AminurAlam <oscorp2004@gmail.com>
2022-01-08 15:40:36 +0530

    adding cover art related stuff

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-import json
-import requests
-import xmltodict
+try: import os, json, requests, xmltodict
+except: print("""packages missing, install them by:
+pip install [package_name]
+
+packages reqiired: json, requests, xmltodict""")
+
+red="\33[31m"
+grn="\33[32m"
+ylw="\33[33m"
+blu="\33[36m"
+wht="\33[00m"
+
 
 def request(searchUrl):
 	data = requests.get(searchUrl).content
 	metadata = json.loads(json.dumps(xmltodict.parse(data)))
 	return metadata
 
-
-def search_artist(query,limit=5,offset=0):
-	base2 = "https://musicbrainz.org/ws/2/artist"
-	searchUrl = base2 + f"?query={query}&limit={limit}"
-	metadata = request(searchUrl)
-	list = metadata["metadata"]["artist-list"]["artist"]
-
-	for i in list:
-		print(type(i),i)
+def get_images(mbid):
+	base = "https://coverartarchive.org/release/"
+	
 
 
 def lookup_rg(mbid,inc="releases"):
@@ -24,28 +27,47 @@ def lookup_rg(mbid,inc="releases"):
 	metadata = request(searchUrl)
 	meta = metadata["metadata"]["release-group"]["release-list"]
 
+#when there is only one release
 	if meta["@count"] == 1 :
 		release = meta["release"]
 
 		try: date = release["date"]
 		except: date = "not found"
+
 		try: country = release["country"]
 		except: country = "not found"
 
-		print(f"""
-id      : {release["@id"]}
-title   : {release["title"]}
-date    : {date}
-country : {country}
-""")
+		try: id = release["@id"]
+		except: id = "not found"
 
+		print(f"""
+[{ylw}{country}{wht}] {release["title"]} [{date}]
+{ylw}{id}{wht}""")
+
+#when there are multiple releassles
 	else:
 		for dic in meta["release"]:
+
+			try: id = dic["@id"]
+			except: id = "not found"
+
+			try: title = dic["title"]
+			except: title = "not found"
+
+			try: date = dic["date"]
+			except: date = red+"0000"
+
+			try: country = dic["country"]
+			except: country = red+"NA"
+
 			print(f"""
-id      : {dic["@id"]}
-title   : {dic["title"]}
-date    : {dic["date"]}
-country : {dic["country"]}""")
+[{ylw}{country}{wht}] {title} [{date}{wht}]
+{ylw}{id}{wht}""")
+
+		for illegal in ["/","\\",":","*","?","\"","<",">"]:
+			title = title.replace(illegal,"_")
+		os.mkdir(f"files/{title}")
+		os.mkdir(f"files/{title}/{dic['@id']}")
 
 
 def search_rg(query,limit=5,offset=0):
@@ -66,19 +88,22 @@ def search_rg(query,limit=5,offset=0):
 				artist = artist + undic["name"] + ", "
 			artist = artist[:-2]
 
+		try: title = dic["title"]
+		except: title = "not found"
+
+		try: id = dic["@id"]
+		except: id = "not found"
+
+		try: type2 = dic["@type"]
+		except: type2 = "not found"
+
 		print(f"""
- {n}
-artist : {artist}
-title  : {dic["title"]}
-id     : {dic["@id"]}
-type   : {dic["@type"]}""")
+{n} {artist} - {title} [{type2}]
+{ylw}{id}{wht}""")
 
 	num = int(input("\n>choose release-group: "))
-	try:
-		mbid = list[num-1]["@id"]
-		lookup_rg(mbid)
-	except:
-		print("error occured, probably due to wrong entry")
+	mbid = list[num-1]["@id"]
+	lookup_rg(mbid)
 
 
 
@@ -88,12 +113,8 @@ def main():
 	#recording, release, release-group, series, work, url
 	print("""
 0 : exit
-
-1 : artist
-2 : lookup artist
-
-3 : release-group
-4 : lookup release-group
+1 : release-group
+2 : lookup release-group
 """)
 
 	entity_type = int(input("search for: "))
@@ -103,12 +124,10 @@ def main():
 
 	if entity_type == 0 : exit()
 
-	elif entity_type == 1: search_artist(query)
-	elif entity_type == 2: None
-
-	elif entity_type == 3: search_rg(query)
-	elif entity_type == 4: lookup_rg(query)
+	elif entity_type == 1: search_rg(query)
+	elif entity_type == 2: lookup_rg(query)
 
 	else: print("the given value doesnt match")
 
-main()
+
+search_rg(input(">enter query: "))
