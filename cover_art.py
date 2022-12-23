0e43da055c44818d91571c2c02a7daf57f5a6401
AminurAlam <oscorp2004@gmail.com>
2022-12-23 19:56:28 +0530

    removed class Config

--- cover_art.py
+++ cover_art.py
@@ -10,9 +10,7 @@ importaint links:
     https://musicbrainz.org/doc/MusicBrainz_API
 """
 
-__version__ = "1.6.0"
-__author__ = "AminurAlam"
-__url__ = "https://github.com/AminurAlam/musicbrainzpy"
+__version__ = "1.6.1"
 
 import os
 import argparse
@@ -36,10 +34,10 @@ def process_releases(releases: list[dict], folder_name: str) -> None:
             path: str = os.path.join(folder_name, file)
             typestr: str = ", ".join(image.get('types'))
 
-            if Config.image == "all" or Config.image.title() in image.get('types'):
+            if args.filter_image == "all" or args.filter_image.title() in image.get('types'):
                 if os.path.exists(path):
                     print(f"     {ylw}[!]{wht} skipping, file exists [{typestr}]")
-                elif Config.dry_run:
+                elif args.dry_run:
                     print(f"     {ylw}[!]{wht} skipping, dry run [{typestr}]")
                 else:
                     # size: str = mbz_api.get_size(link)
@@ -50,78 +48,70 @@ def process_releases(releases: list[dict], folder_name: str) -> None:
 
 
 def search_rg(query: str) -> tuple[list, str]:
-    rgs: list[dict] = mbz_api.search_release_group(query, Config.limit, 0)['release-groups']
+    rgs: list[dict] = mbz_api.search_release_group(query, args.limit, 0)['release-groups']
 
-    if Config.search != "all":  # filtering the search results we got using -fs / --filter-search
-        rgs = list(filter(lambda rg: rg.get('primary-type', '').lower() == Config.search, rgs))
+    if args.filter_search != "all":  # filtering the search results
+        rgs = list(filter(lambda rg: rg.get('primary-type', '').lower() == args.filter_search, rgs))
 
     if not rgs:  # exits if everything gets filtered out
         raise Exception("no search results, try changing filter or increasing limit")
 
-    rgs.sort(reverse=True, key=lambda rg: rg['score']*len(rg['releases']))  # sorting so first item is top result
+    rgs.sort(reverse=True, key=lambda rg: rg['score'] * len(rg['releases']))
 
     for n, rg in enumerate(rgs, start=1):
-        # [name['name'] for name in rg['artist-credit']])
-        # [rg.get('primary-type', 'none')].extend(rg.get('secondary-types', []))
-        print(f"[{ylw}{n}{wht}] {grn}{', '.join([name['name'] for name in rg['artist-credit']])}", end="")
-        print(f" - {rg.get('title')}{wht} ({rg['count']} {rg.get('primary-type', 'none')})")
+        artists: str = ", ".join([name['name'] for name in rg['artist-credit']])
+        types: str = ", ".join([rg.get('primary-type', 'none')] + rg.get('secondary-types', []))
+        print(f"[{ylw}{n}{wht}] {grn}{artists} - {rg.get('title')}{wht} ({rg['count']} {types})")
 
     choice: str = input(f"{grn}\n>choose release-group: {wht}")
     print('\x1b[1A\x1b[2K' * (len(rgs) + 2))  # clearing screen/search results
-    if choice == "0": exit()
-    rg: dict = rgs[0] if choice == "" else rgs[int(choice)-1]
 
+    if choice == "0":
+        exit()
+
+    rg: dict = rgs[0] if choice == "" else rgs[int(choice) - 1]
     name: str = f"{rg['artist-credit'][0]['name']} - {rg['title']}".replace("/", '-')
-    rg_folder_name = os.path.join(Config.path, name)
-
-    if not os.path.exists(rg_folder_name) and not Config.dry_run: os.makedirs(rg_folder_name)
-
+    rg_folder_name = os.path.join(args.outdir, name)
     print(f"{name} ({rg['count']})\nhttps://musicbrainz.org/release-group/{rg['id']}\n")
+
+    if not os.path.exists(rg_folder_name) and not args.dry_run:
+        os.makedirs(rg_folder_name)
 
     return rg['releases'], rg_folder_name
 
 
-def get_args() -> tuple:
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("query",
         help="name of the album")
     parser.add_argument("-l", "--limit",
         help="max number of results displayed",
         type=int,
+        default=5,
         metavar="NUM")
     parser.add_argument("-n", "--dry-run",
         help="run without downloading artwork",
+        default=False,
         action="store_true")
     parser.add_argument("-o", "--outdir",
         help="change the output directory",
         type=str,
+        default="Covers",
         metavar="PATH")
-    parser.add_argument("-fi", "--filter-image",
+    parser.add_argument("-i", "--filter-image",
         help="filter images",
         type=str,
+        default="front",
         metavar="TYPE",
         choices=["all", "front", "booklet", "medium"])
-    parser.add_argument("-fs", "--filter-search",
+    parser.add_argument("-s", "--filter-search",
         help="filter search results",
         type=str,
+        default="all",
         metavar="TYPE",
         choices=["all", "album", "single", "ep", "broadcast", "other"])
 
     args = parser.parse_args()
-
-    class Config:
-        limit = args.limit or 5               # number of results shown
-        dry_run = args.dry_run or False       # run without downlading
-        path = args.outdir or "Covers"        # folder to download in
-        image = args.filter_image or "front"  # all/front/back/booklet/obi/medium
-        search = args.filter_search or "all"  # https://musicbrainz.org/doc/Release_Group/Type
-
-    return Config, args.query
-
-
-if __name__ == "__main__":
-    Config, query = get_args()
     grn, ylw, wht = ["\033[32m", "\033[33m", "\033[00m"] if os.name == "posix" else [""] * 3
 
-    try: process_releases(*search_rg(query))
-    except KeyboardInterrupt: exit()
+    process_releases(*search_rg(args.query))
